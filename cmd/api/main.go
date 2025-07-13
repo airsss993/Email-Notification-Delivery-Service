1e4e5dd01911311e9bc0a26774ac4f43ac59cbbe
Artem <dzaparidzeartem@gmail.com>
2025-07-13 22:34:54 +0500

    Add Gin web server with /ping endpoint
    Integrated Gin as the HTTP server and added a /ping endpoint for health checks. Updated dependencies in go.mod and go.sum to include Gin and related packages.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"github.com/airsss993/email-notification-service/internal/logger"
+	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	logger.Init()
 	//cfg := config.Load()
-	log.Info().Msg("Service started successfully")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := router.Run()
+	if err != nil {
+		log.Fatal().Msg("Failed to start the service: " + err.Error())
+	} else {
+		log.Info().Msg("Service started on port :8080")
+	}
+
 }
