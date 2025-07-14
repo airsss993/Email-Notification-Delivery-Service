a07ac1fe5c4db5a162ae0430aba632c5dd64cd75
Artem <dzaparidzeartem@gmail.com>
2025-07-14 13:34:50 +0500

    Refactor project structure and routing setup
    Moved handler, routes, and database connection logic to new internal packages for improved modularity. Removed obsolete files from internal/api and internal/database, and added new model, repository, router, and service packages for template handling. Updated main.go and config.go to use the new structure and environment variable for database connection.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,30 +1,37 @@
 package main
 
 import (
-	"github.com/airsss993/email-notification-service/internal/api"
-	"github.com/airsss993/email-notification-service/internal/database"
+	"database/sql"
+	"github.com/airsss993/email-notification-service/internal/config"
 	"github.com/airsss993/email-notification-service/internal/logger"
-	"github.com/gin-gonic/gin"
+	"github.com/airsss993/email-notification-service/internal/router"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/rs/zerolog/log"
+	"os"
 )
 
 func main() {
-	// Initialize logger
+	// Load .env
+	_ = godotenv.Load()
+
+	// Initialize logger and configuration
 	logger.Init()
+	config.Load()
 
-	r := gin.New()
-	r.Use(gin.Recovery(), logger.CustomLogger())
-
-	api.SetupRoutes(r)
-
-	_, err := database.DBConn()
+	// Initialize database connection
+	connStr := os.Getenv("DB_URL")
+	_, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to connect to the database")
-		return
 	} else {
 		log.Info().Msg("Database connection established successfully")
 	}
-	
+
+	// Setup router
+	r := router.SetupRouter()
+
+	// Start the server
 	err = r.Run()
 	if err != nil {
 		log.Fatal().Msg("Failed to start the service: " + err.Error())
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,39 +1,13 @@
 package config
 
-import (
-	"github.com/joho/godotenv"
-	"github.com/rs/zerolog/log"
-	"os"
-)
+import "os"
 
 type Config struct {
-	DbPort string
-	DbHost string
-	DbName string
-	DbUser string
+	DatabaseURL string
 }
 
-func Load() *Config {
-	cfg := &Config{
-		DbPort: checkEnv("DB_PORT"),
-		DbHost: checkEnv("DB_HOST"),
-		DbName: checkEnv("DB_NAME"),
-		DbUser: checkEnv("DB_USER"),
+func Load() Config {
+	return Config{
+		DatabaseURL: os.Getenv("DATABASE_URL"),
 	}
-
-	log.Info().Msg("Configuration loaded successfully")
-	return cfg
-}
-
-func checkEnv(key string) string {
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		log.Err(err)
-	}
-
-	val := os.Getenv(key)
-	if val == "" {
-		log.Fatal().Msg("Environment variable " + key + " is not set")
-	}
-	return val
 }
